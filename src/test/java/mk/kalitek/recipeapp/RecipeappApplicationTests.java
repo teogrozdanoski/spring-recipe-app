9323295d7369f6cc532b01c6e1604756c4208c4e
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-12-01 18:55:33 +0100

    initial project commit

---
+++ src/test/java/mk/kalitek/recipeapp/RecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.kalitek.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
